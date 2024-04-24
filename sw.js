78b913594a0c5fbbe61313aa0e068ad56a066e15
Christian Chávez <kevin.chavez@yachaytech.edu.ec>
2024-04-23 22:47:39 -0500

    Updates

--- sw.js
+++ sw.js
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "01975a3f5d7c16620e7bb531d9d9a6dc"
+    "revision": "04dbbd77f1164bb8b7b8c815a6e84c91"
   },
   {
     "url": "manifest.webmanifest",
